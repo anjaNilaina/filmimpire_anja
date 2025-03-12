5cf4763523df2148df4d24cc5d16d4e5c756115c
anja <anjahnilaina@gmail.com>
2025-03-12 22:50:06 +0300

    just navbar but lol

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { AppBar, Toolbar, IconButton, Button , Drawer , Avatar,useMediaQuery} from '@mui/material'
+import { Link } from 'react-router-dom'
+import { AccountCircle, Menu , Brightness4, Brightness7 } from '@mui/icons-material'
+
+import { useStyles } from './styles'
+import { useTheme } from '@mui/material/styles'
+
+
 
 const Navbar = () => {
+
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)')
+  const theme = useTheme();
+
   return (
-    <div>Navbar</div>
+
+    <>
+      <AppBar position="fixed">
+
+        <Toolbar className={classes.toolbar}>
+          
+          { isMobile && ( 
+
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                oncClick = {() => { }}
+                className={classes.menuButton}
+              >
+
+                <Menu/>
+              </IconButton>
+
+          ) }
+
+          <IconButton color="inherit"  > 
+            {theme.palette.mode === 'dark' ? <Brightness4/> : <Brightness7/>}
+          </IconButton>
+         
+
+       
+         </Toolbar> 
+
+
+      </AppBar>
+    
+    </>
+
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { createTheme , ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({})
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <ThemeProvider theme = {theme}>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+    </ThemeProvider>
+
 )
